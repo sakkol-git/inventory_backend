c71b6b5274f1ad44238961ff4ba69ddbbee4a9fd
Chen Sakkol <chensakkol1124@gmail.com>
2026-05-18 18:07:21 +0700

    remove soft delete from transaction

--- app/Modules/Inventory/Models/Transaction.php
+++ app/Modules/Inventory/Models/Transaction.php
@@ -13,12 +13,11 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\MorphTo;
-use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Transaction extends Model
 {
     /** @use HasFactory<TransactionFactory> */
-    use HasFactory, SoftDeletes;
+    use HasFactory;
 
     protected $table = 'transactions';
 
