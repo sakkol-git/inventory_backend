284709998eabcb0280c9a028359ab4110403ff5e
sakkol-git <chensakkol1124@gmail.com>
2026-06-17 20:26:43 +0700

    improve relationship of Document and achievement 3

--- app/Modules/Inventory/Models/Achievement.php
+++ app/Modules/Inventory/Models/Achievement.php
@@ -11,6 +11,7 @@ use Database\Factories\AchievementFactory;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Achievement extends Model
 {
